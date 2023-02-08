823554e6c03cb3f236fb168f3d2df5f54ccdb328
Sergey <sergey.p@zimalab.com>
2023-02-08 21:14:18 +0700

    Add client entity

---
+++ src/Entity/Client.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column]
+    private string $firstName;
+
+    #[ORM\Column]
+    private string $lastName;
+
+    #[ORM\Column(length: 12)]
+    private string $phoneNumber;
+
+    #[ORM\Column]
+    private string $email;
+
+    #[ORM\Column]
+    private bool $consentProcessingPersonalData;
+
+    #[ORM\Column]
+    private int $score;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function isConsentProcessingPersonalData(): bool
+    {
+        return $this->consentProcessingPersonalData;
+    }
+
+    public function setConsentProcessingPersonalData(bool $consentProcessingPersonalData): self
+    {
+        $this->consentProcessingPersonalData = $consentProcessingPersonalData;
+        return $this;
+    }
+
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+        return $this;
+    }
+}
